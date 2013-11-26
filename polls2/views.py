a6b9badc01d647b97c10b3aa388d71f83ab1cbd6
itgkbaba <itgk.baba@gmail.com>
2013-11-26 20:07:50 +0900

    view 3回目
    indexのview、shortcut版作成

--- polls2/views.py
+++ polls2/views.py
@@ -1,14 +1,12 @@
 from django.template import Context, loader
+from django.shortcuts import render_to_response
 from polls2.models import Poll
 from django.http import HttpResponse
 
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-    t = loader.get_template('polls2/index.html')
-    c = Context({
-        'latest_poll_list':latest_poll_list,
-    })
-    return HttpResponse(t.render(c))
+    return render_to_response('polls2/index.html',
+                             {'latest_poll_list': latest_poll_list})
 
 def detail(request, poll_id):
     return HttpResponse("You're looking at poll %s." % poll_id)
