e8f5f8f7371e3b81f4032a4745b90190a1e43f10
itgkbaba <itgk.baba@gmail.com>
2013-11-26 20:24:59 +0900

    view 5回目
    detailのviewすっきりverへ

--- polls2/views.py
+++ polls2/views.py
@@ -1,6 +1,5 @@
 from django.template import Context, loader
-from django.shortcuts import render_to_response
-from django.http import Http404
+from django.shortcuts import render_to_response, get_object_or_404
 from polls2.models import Poll
 from django.http import HttpResponse
 
@@ -10,11 +9,8 @@ def index(request):
                              {'latest_poll_list': latest_poll_list})
 
 def detail(request, poll_id):
-    try:
-        p = Poll.objects.get(pk=poll_id)
-    except Poll.DoesNotExist:
-        raise Http404
-    return render_to_response('polls2/detail.html',{'poll': p})
+    p = get_object_or_404(Poll, pk=poll_id)
+    return render_to_response('polls2/detail.html', {'poll': p})
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
